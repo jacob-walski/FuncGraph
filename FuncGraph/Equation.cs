175495ffd02503e687c02bfa9891838fe27c5f1b
jacob-walski <59016282+jacob-walski@users.noreply.github.com>
2019-12-18 13:32:48 +0100

    v1.0

---
+++ FuncGraph/Equation.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncGraph
+{
+    class Equation
+    {
+        private string Infix; //rownanie w konwencjonalnym zapisie algebraicznym - infixowym
+        private Queue<string> Postfix; //rownanie po przekladzie do odwrotnej notacji polskiej - zapis postfixowy
+
+        public Equation(string equation)
+        {
+            this.Infix = equation;
+            Converse(Tokenize(this.Infix));
+        }
+
+        private Queue<string> Tokenize(string text) //tokenizacja - podzial poszczegolnych elementow stringa wejsciowego, na liczby, operatory, funkcje matematyczne
+        {
+            string temp = "";
+            double number;
+            Queue<string> queue = new Queue<string>();
+
+            foreach(char c in text) //czytanie wejscia znak po znaku
+            {
+                if (c == ' ') //ignorowanie bialych znakow
+                    continue;
+
+                switch (c)
+                {
+                    case '+': //odczytywanie operatorow
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '^':
+                    case '(':
+                    case ')':
+                        if(double.TryParse(temp, out number))
+                        {
+                            queue.Enqueue(temp);
+                            temp = "";
+                        }
+                        else if(temp != "")
+                        {
+                            queue.Enqueue(temp);
+                            temp = "";
+                        }
+                        queue.Enqueue(c.ToString());
+                        break;
+
+                    case '0': //odczytywanie cyfr oraz separatorow
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                    case '.':
+                    case ',':
+                        temp += c.ToString();
+                        break;
+
+                    default: //odczytywanie funkcji
+                        if(Char.IsLetter(c))
+                        {
+                            if (double.TryParse(temp, out number))
+                            {
+                                queue.Enqueue(temp);
+                                queue.Enqueue("*");
+                                temp = "";
+                            }
+
+                            temp += c.ToString();
+                        }
+                        break;
+                }
+            }
+
+            if(temp != "") //umieszczenie ostatniego odczytanego elementu do kolejki przechowujacej wyrazenie w postaci infixowej
+            {
+                queue.Enqueue(temp);
+            }
+
+            return queue;
+        }
+
+        private int Priory(string operat) //ustalenie priorytetow poszczegolnych operatorow - kolejnosc wykonywania dzialan
+        {
+            switch(operat)
+            {
+                case "(":
+                    return 0;
+                case "+":
+                case "-":
+                case ")":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                case "^":
+                case "sqrt":
+                    return 3;
+                case "sin":
+                case "cos":
+                case "tg":
+                case "ctg":
+                    return 4;
+            }
+
+            return -1;
+        }
+        private void Converse(Queue<string> infix) //konwersja zapisu infix do postfix
+        {
+            string temp;
+
+            Queue<string> queue = new Queue<string>(); //wg algorytmu konwersji tworzona jest kolejka oraz stos
+            Stack<string> stack = new Stack<string>();
+
+            while(infix.Any()) //algorytm konwersji
+            {
+                temp = infix.Dequeue();
+                switch(Priory(temp))
+                {
+                    case -1:
+                        queue.Enqueue(temp);
+                        break;
+                    case 0:
+                        stack.Push(temp);
+                        break;
+                    case 1:
+                        if(temp == ")")
+                        {
+                            while(stack.Peek() != "(")
+                            {
+                                queue.Enqueue(stack.Pop());
+                            }
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            while(stack.Any() && Priory(temp) <= Priory(stack.Peek()))
+                            {
+                                queue.Enqueue(stack.Pop());
+                            }
+                            stack.Push(temp);
+                        }
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        if(temp == "^") //potega jest wyjatkiem poniewaz jest operatorem prawostronnie łącznym tzn. 3^2^4 = 3^(2^4)
+                        {
+                            while(stack.Any() && Priory(temp) < Priory(stack.Peek()))
+                            {
+                                queue.Enqueue(stack.Pop());
+                            }
+                        }
+                        else
+                        {
+                            while (stack.Any() && Priory(temp) <= Priory(stack.Peek()))
+                            {
+                                queue.Enqueue(stack.Pop());
+                            }
+                        }
+                        stack.Push(temp);
+                        break;
+                }
+            }
+
+            while(stack.Any())
+            {
+                queue.Enqueue(stack.Pop());
+            }
+
+            this.Postfix = queue;
+        }
+
+        public double Compute(double x) //wyliczenie wartosci funkcji dla konkretnej wartosci x
+        {
+            Queue<string> queue = new Queue<string>(this.Postfix);
+            Stack<double> stack = new Stack<double>();
+
+            double number;
+
+            while(queue.Any())
+            {
+                switch (Priory(queue.Peek()))
+                {
+                    case -1:
+                        if (Char.IsLetter(queue.Peek()[0]))
+                        {
+                            queue.Dequeue();
+                            stack.Push(x);
+                        }
+                        else
+                        {
+                            stack.Push(double.Parse(queue.Dequeue()));
+                        }
+                        break;
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        if (queue.Peek() == "sqrt")
+                        {
+                            stack.Push(Math.Sqrt(stack.Pop()));
+                        }
+                        else
+                        {
+                            number = stack.Pop();
+                            switch (queue.Peek())
+                            {
+                                case "+":
+                                    number += stack.Pop();
+                                    break;
+                                case "-":
+                                    number = stack.Pop() - number;
+                                    break;
+                                case "*":
+                                    number *= stack.Pop();
+                                    break;
+                                case "/":
+                                    number = stack.Pop() / number;
+                                    break;
+                                case "^":
+                                    number = Math.Pow(stack.Pop(), number);
+                                    break;
+                            }
+                            stack.Push(number);
+                        }
+                        queue.Dequeue();
+                        break;
+                    case 4:
+                        number = stack.Pop();
+                        switch (queue.Peek())
+                        {
+                            case "sin":
+                                stack.Push(Math.Sin(number));
+                                break;
+                            case "cos":
+                                stack.Push(Math.Cos(number));
+                                break;
+                            case "tg":
+                                stack.Push(Math.Tan(number));
+                                break;
+                            case "ctg":
+                                number = 1 / Math.Tan(number);
+                                stack.Push(number);
+                                break;
+                        }
+                        queue.Dequeue();
+                        break;
+                }
+            }
+            
+            return stack.Pop();
+        }
+    }
+}
---
+++ FuncGraph/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FuncGraph
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw(ref Canvas canvas, Equation equation, double minX, double maxX)
+        {
+            int steps = 200; //liczba linii odwzorowywujących wykres funkcji
+            double functionInterval = (maxX - minX) / steps; //odstep pomiedzy kolejnymi punktami funkcji - te punkty sa pozniej laczone za pomoca linii tworzac wykres
+
+            double minY = double.MaxValue;
+            double maxY = double.MinValue;
+            double x;
+            double y;
+
+            for (int i = 0; i < steps; ++i) //obliczanie minuma i maksima funkcji w danym zakresie x
+            {
+                x = (functionInterval * i) + minX;
+                y = equation.Compute(x);
+                if (y < minY)
+                {
+                    minY = y;
+                }
+                else if (y > maxY)
+                {
+                    maxY = y;
+                }
+            }
+
+            Line line;
+            double screenInterval = canvas.ActualWidth / steps; //odstep na ekranie wyrazony w pikselach pomiedzy kolejnymi badanymi wartosciami na osi x
+
+            double y1;
+            double y2;
+
+            for (int i = 0; i < steps; ++i) //wyliczenie wspolrzednych punktu wykresu oraz ich wyskalowanie
+            {
+                line = new Line();
+                line.Stroke = Brushes.DarkMagenta;
+
+                line.X1 = screenInterval * i;
+                line.X2 = screenInterval * (i + 1);
+
+                y1 = equation.Compute((functionInterval * i) + minX);
+                y1 -= minY;
+                y1 /= (maxY - minY);
+                y1 = 1 - y1;
+                line.Y1 = canvas.ActualHeight * y1;
+
+                y2 = equation.Compute((functionInterval * (i + 1)) + minX);
+                y2 -= minY;
+                y2 /= (maxY - minY);
+                y2 = 1 - y2;
+                line.Y2 = canvas.ActualHeight * y2;
+
+                canvas.Children.Add(line);
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Equation eq = new Equation(TextBox1.Text); //stworzenie rownania w oparciu o tresc wprowadzana przez uzytkownika
+            Canvas1.Children.Clear(); //czyszczenie zawartosci kontrolki canvas odpwiedzialnej za wyswietlanie wykresu
+            Draw(ref Canvas1, eq, double.Parse(TextBox2.Text), double.Parse(TextBox3.Text)); //przekazywanie referencji do kontrolki canvas celem narysowania wykresu o okreslonych parametrach
+        }
+    }
+}
